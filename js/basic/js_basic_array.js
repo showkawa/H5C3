f5243840cca7a2faaccf593b3b8e332927e817a6
brian-root <1092249319@qq.com>
2022-11-01 22:06:47 +0800

    some(), every(),flat(),flatMap()

--- js/basic/js_basic_array.js
+++ js/basic/js_basic_array.js
@@ -20,10 +20,20 @@ console.log(arrFind)
 const arrFindIndex = arr.findIndex(val => val % 2 === 0)
 console.log(arrFindIndex)
 
-//some
+//some() 方法测试数组中是不是至少有 1 个元素通过了被提供的函数测试。它返回的是一个 Boolean 类型的值。
+const arrSome = arr.some(val => val > 4)
+console.log('some()', arrSome)
 
-//every
+//every() 方法测试一个数组内的所有元素是否都能通过某个指定函数的测试。它返回一个布尔值。
+const arrEvery = arr.every(val => val > 4)
+console.log('every()', arrEvery)
 
-//flat
+const arr2 = [[1, 2, 3], [4, [5, 6]], 7]
+//flat() 方法会按照一个可指定的深度递归遍历数组，并将所有元素与遍历到的子数组中的元素合并为一个新数组返回。
+console.log(arr2.flat(2))
 
-//flatMap
+
+const arr3 = ['this', 'is', 'a', 'new man']
+//flatMap() 方法首先使用映射函数映射每个元素，然后将结果压缩成一个新数组。它与 map 连着深度值为 1 的 flat 几乎相同，但 flatMap 通常在合并成一种方法的效率稍微高一些。
+const arrFM = arr3.flatMap(val => val.split(" "))
+console.log(arrFM)

c93e81f49449bdc9c79c23cc546986a1ac3ed326
brian-root <1092249319@qq.com>
2022-11-04 21:26:06 +0800

    math API

---
+++ js/basic/js_basic_math.js
@@ -0,0 +1,26 @@
+//Math.sqrt() 函数返回一个数的平方根
+console.log(Math.sqrt(9))
+console.log(4 ** (1 / 2))
+console.log(27 ** (1 / 3))
+
+//Math.max() 函数返回作为输入参数的最大数字，如果没有参数，则返回 -Infinity。
+console.log(Math.max('9', 2, 3, 5))
+
+//Math.min() 函数返回作为输入参数的数字中最小的一个，如果没有参数，则返回 Infinity。
+console.log(Math.min('-3', -1, 0, 5))
+
+
+//Math.PI 表示一个圆的周长与直径的比例，约为 3.14159：
+console.log(Math.PI.toFixed(2))
+
+//Math.trunc() 方法会将数字的小数部分去掉，只保留整数部分。
+console.log(Math.trunc(Math.PI))
+
+//Math.round() 函数返回一个数字四舍五入后最接近的整数。
+console.log(Math.round(3.56))
+console.log(Math.round(-3.56))
+
+//Math.floor() 函数总是返回小于等于一个给定数字的最大整数。
+console.log(Math.floor(32.1), Math.floor(-32.1))
+
+console.log(45_1430_0000)

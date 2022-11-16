f78503f6c7677e2f79d6877f684e29f7d69285e2
brian-root <1092249319@qq.com>
2022-11-16 22:48:34 +0800

    IntersectionObserver

--- js/basic/observer/index.js
+++ js/basic/observer/index.js
@@ -2,25 +2,22 @@
 
 const title = document.querySelector('.title')
 const content = document.querySelector('.content')
-const mh = title.getBoundingClientRect().height;
+const mh = content.getBoundingClientRect().height;
 
 const obsCallback = (entries, observer) => {
     const [entry] = entries;
     if (entry.isIntersecting) {
         title.classList.add('fixed')
-        console.log(title.getBoundingClientRect())
+        console.log(entry)
     } else {
         title.classList.remove('fixed')
     }
-
-    entries.forEach(entry => console.log(entry))
-
 }
 const obsOptions = {
     root: null,
     threshold: 0,
-    rootMargin: `-${mh}px`
+    // rootMargin: `-${mh}px`
 }
 const observer = new IntersectionObserver(obsCallback, obsOptions)
-observer.observe(title)
+observer.observe(content)
 

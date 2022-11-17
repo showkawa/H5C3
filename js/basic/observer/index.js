fe49e5475a0962b0f3f3d675b39b2e5af4915ee2
brian-root <1092249319@qq.com>
2022-11-17 22:01:04 +0800

    DOMContentLoaded and load event

--- js/basic/observer/index.js
+++ js/basic/observer/index.js
@@ -21,3 +21,8 @@ const obsOptions = {
 const observer = new IntersectionObserver(obsCallback, obsOptions)
 observer.observe(content)
 
+// load,这个事件仅仅应该在探测到整个页面完全加载完成时被使用
+window.addEventListener('load',() => console.log('load'))
+// 当纯 HTML 被完全加载以及解析时，**DOMContentLoaded **事件会被触发，而不必等待样式表，图片或者子框架完成加载。
+document.addEventListener('DOMContentLoaded',() => console.log('DOMContentLoaded'))
+

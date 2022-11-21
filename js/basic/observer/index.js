d4dc082c3d77dc567e874dd8a64a9a68056a0ed1
brian-root <1092249319@qq.com>
2022-11-21 22:12:39 +0800

    private filed, private method, static method

--- js/basic/observer/index.js
+++ js/basic/observer/index.js
@@ -22,7 +22,56 @@ const observer = new IntersectionObserver(obsCallback, obsOptions)
 observer.observe(content)
 
 // load,这个事件仅仅应该在探测到整个页面完全加载完成时被使用
-window.addEventListener('load',() => console.log('load'))
+window.addEventListener('load', () => console.log('load'))
 // 当纯 HTML 被完全加载以及解析时，**DOMContentLoaded **事件会被触发，而不必等待样式表，图片或者子框架完成加载。
-document.addEventListener('DOMContentLoaded',() => console.log('DOMContentLoaded'))
+document.addEventListener('DOMContentLoaded', () => console.log('DOMContentLoaded'))
 
+
+// OOP
+class User {
+    name = "qwe"
+    kawa = "asda"
+    age = 12
+    // private filed
+    #role = "VP1"
+
+    getRole() {
+        return this.#role;
+    }
+
+    // private method
+    #updateRole() {
+        return this.#role = "SVP";
+    }
+
+    getNewRole() {
+        return this.#updateRole()
+    }
+    // static method
+    static roles(...roles) {
+        return [...roles]
+    }
+
+    //chain method
+    addPreName(pre) {
+         this.name = pre + this.name;
+         return this;
+    }
+    updateAge(num){
+        this.age = num;
+        return this;
+    }
+
+}
+User.prototype.getage = function () {
+    console.log(this)
+    return this.age;
+}
+
+const user = new User();
+user.getage();
+console.log(user.getRole());
+console.log(user.getNewRole())
+
+console.log(User.roles('VP2', 'VP3'))
+console.log(user.addPreName('Brian ').updateAge(31))
